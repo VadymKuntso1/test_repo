c53662a2a822c68f9f12619b157e898028946c8b
VadymKuntso1 <vadym.kuntso@selfinspection.com>
2024-07-28 18:00:52 +0300

    Clear branch

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git commit -m 'deploy'
 
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
-git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:https://vadymkuntso1.github.io/test_repo.git master:gh-pages
 
 cd -
