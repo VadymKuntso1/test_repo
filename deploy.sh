663e9e34391848e8cb480a761c256ef5da8e8979
VadymKuntso1 <vadym.kuntso@selfinspection.com>
2024-07-28 17:56:54 +0300

    ci\cd settings

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+set -e
+
+npm run build
+
+cd dist
+
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+
+cd -
